7e6ecc618a798300250127a935b70ab75480ba38
g100rabh <gsaurabh909@gmai.com>
2023-05-30 23:15:39 +0530

    promiseAllupdated

--- promiseAll_main.js
+++ promiseAll_main.js
@@ -1,40 +1,88 @@
+// const posts = [{title: 'POST1'}];
+// //Do not touch this function
+// var currentDate = new Date();
+// function printPost() {
+//     posts.forEach((post) => {
+//         console.log(post.title)
+//     })
+// }
+// function create2ndPost() {
+//     return new Promise( (resolve, reject) => {
+//         setTimeout( () => {
+//             posts.push({title: 'POST2'});
+//             currentDate = new Date();
+//             resolve()
+//         }, 3000, console.log(`Last user Activity: ${currentDate}`))
+//     }) 
+// }
+// //Do not touch this function
+// function create3rdPost() {
+//     return new Promise( (resolve, reject) => {
+//         setTimeout( () => {
+//             posts.push({title: 'POST3'});
+//             resolve();
+//         }, 2000, console.log(`Last user Activity: ${currentDate}`))
+//     }) 
+// }
+// function updateLastUserActivityTime() {
+//     return new Promise( (resolve, reject) => {
+//         setTimeout( () => {
+//             currentDate = new Date();
+//             let time = currentDate.getTime();
+//             console.log(`updateLastUserActivityTime: ${time}`)
+//             resolve()
+//         }, 1000)
+//     })
+// }
+
+// //Do not touch this function
+// function deletePost(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout( () => {
+//             if(posts.length > 0){
+//                 const poppedElement  = posts.pop();
+//                 resolve(poppedElement);
+//             } else {
+//                 reject("ERROR: ARRAY IS EMPTY")
+//             }
+//         }, 1000)
+//     })
+// }
+
+// create2ndPost()
+//     .then(updateLastUserActivityTime)
+//     .then(create3rdPost)
+//     .then(updateLastUserActivityTime)
+//     .then(deletePost)
+//     .then(printPost);
+
 const posts = [{title: 'POST1'}];
-//Do not touch this function
-var currentDate = new Date();
+const currentDate = new Date();
 function printPost() {
-    posts.forEach((post) => {
-        console.log(post.title)
-    })
+    return new Promise( (resolve, reject)  => {
+        resolve(posts.forEach((post) => {
+            console.log(post.title)})
+    )})
 }
 function create2ndPost() {
     return new Promise( (resolve, reject) => {
         setTimeout( () => {
             posts.push({title: 'POST2'});
-            resolve()
+            resolve(new Date())
         }, 3000, console.log(`Last user Activity: ${currentDate}`))
     }) 
 }
-//Do not touch this function
-function create3rdPost() {
-    return new Promise( (resolve, reject) => {
-        setTimeout( () => {
-            posts.push({title: 'POST3'});
-            resolve();
-        }, 2000, console.log(`Last user Activity: ${currentDate}`))
-    }) 
-}
+
 function updateLastUserActivityTime() {
     return new Promise( (resolve, reject) => {
         setTimeout( () => {
-            currentDate = new Date();
-            let time = currentDate.getTime;
-            console.log(`updateLastUserActivityTime: ${time}`)
-            resolve()
+            const currentDate = new Date();
+            let time = currentDate.getTime();
+            resolve(time)
         }, 1000)
     })
 }
 
-//Do not touch this function
 function deletePost(){
     return new Promise((resolve, reject) => {
         setTimeout( () => {
@@ -48,9 +96,9 @@ function deletePost(){
     })
 }
 
-create2ndPost()
-    .then(updateLastUserActivityTime)
-    .then(create3rdPost)
-    .then(updateLastUserActivityTime)
-    .then(deletePost)
+Promise.all([create2ndPost(),updateLastUserActivityTime(),deletePost(),updateLastUserActivityTime()])
+    .then((res) => { 
+        console.log(res)
+    })
     .then(printPost);
+
